ddb5781c6b8edf12125ec35358f7b891846f6e3b
Catch-25 <backupvaporwave@gmail.com>
2025-10-13 12:34:01 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,9 +1,11 @@
+#This list finds two smallest numbers of the list and displays results in txt. file
+
 import myfunctions
 import test_data
 
 with open('result.txt', 'w') as f:
     for i, lst in enumerate([test_data.list1, test_data.list2, test_data.list3], 1):
         smallest, second = myfunctions.find_two_smallest(lst)
-        line = f"List {i}: 2 smaillest: {smallest}, {second}\n"
+        line = f"List {i}: Smallest two: {smallest}, {second}\n"
         f.write(line)
         print(line.strip())
